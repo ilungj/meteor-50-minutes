cb86cc8760bc302a801e4ec1c554d0c2b2027c5e
Il Ung <ilungj@gmail.com>
2017-10-07 22:37:56 -1000

    Task 3: Tasklist form

--- app/tasklist.js
+++ app/tasklist.js
@@ -6,6 +6,30 @@ if (Meteor.isClient) {
       return Tasks.find({}, {sort: {createdAt: -1}});
     }
   });
+
+  Template.tasks.events({
+    "submit .add-task": function(event) {
+      var name = event.target.name.value;
+
+      Tasks.insert({
+        name: name,
+        createdAt: new Date(),
+        userId: Meteor.userId()
+      });
+
+      event.target.name.value = '';
+
+      return false;
+    },
+
+    "click .delete-task": function() {
+      if (confirm('Delete Task?')) {
+        Tasks.remove(this._id);
+      }
+
+      return false;
+    }
+  });
 }
 
 if (Meteor.isServer) {
